5da6738e14f5575a91d9b9a2624970bac1ddfe83
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-09-21 19:24:59 +0200

    Add Text background and foreground color

--- Sources/khake/ui/Text.hx
+++ Sources/khake/ui/Text.hx
@@ -2,27 +2,59 @@ package khake.ui;
 
 import khake.Entity;
 
-import kha.Font;
 import kha.graphics2.Graphics;
+import kha.Color;
+import kha.Font;
 
+/**
+    A rudimentary text label UI element.
+**/
 class Text extends Entity {
+    /**
+        Text font family.
+    **/
     public var font : Font;
-    
+
+    /**
+        Text background color.
+    **/
+    public var backgroundColor : Color;
+
+    /**
+        Text foreground color.
+    **/
+    public var foregroundColor : Color;
+
+    /**
+        Text font size.
+    **/
     var size : Int;
+
+    /**
+        Text value.
+    **/
     var value : String; 
 
     public function new(font : Font, size : Int) {
         super(0, 0, 0, 0);
         this.font = font;
         this.size = size;
+        
+        this.backgroundColor = Color.fromValue(0x00000000);
+        this.foregroundColor = Color.White;
         this.value = "";
         updateDimensions();
     }
 
     override public function draw(g : Graphics) : Void {
+        super.draw(g);
+        g.color = this.backgroundColor;
+        g.fillRect(this.x, this.y, this.width, this.height);
+        g.color = this.foregroundColor;
         g.font = this.font;
         g.fontSize = this.size;
         g.drawString(this.value, this.x, this.y);
+        g.color = Color.White;
     }
 
     public function setSize(size : Int) : Void {
