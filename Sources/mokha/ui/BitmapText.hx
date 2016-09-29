7827a227f8f8622c84c559193cb815d163994200
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-09-29 13:38:07 +0200

    Add support for monospaced bitmap fonts

---
+++ Sources/mokha/ui/BitmapText.hx
@@ -0,0 +1,28 @@
+package mokha.ui;
+
+import mokha.ui.BitmapFont;
+import mokha.Entity;
+
+import kha.graphics2.Graphics;
+import kha.Color;
+
+class BitmapText extends Entity {
+	public var bitmapFont : BitmapFont;
+	public var value : String;
+
+	public function new(value : String, bitmapFont : BitmapFont) : Void {
+		super(0, 0, 0, 0);
+
+		this.value = value;
+		this.bitmapFont = bitmapFont;
+	}
+
+	override public function draw(g : Graphics) : Void {
+		super.draw(g);
+
+		g.color = Color.White;
+		for (i in 0...value.length) {
+			bitmapFont.drawChar(g, x + i * bitmapFont.charWidth, y, value.charAt(i));
+		}
+	}
+}
