7da8e680f933a96edd914695aafa53a07e7d8ced
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-10-18 21:16:32 +0200

    Drop layer collection, integrate it with state
    The `Layer` collection has been dropped in favour of its integration with
    the `State` type. A state handles its objects directly.

--- Sources/mokha/State.hx
+++ Sources/mokha/State.hx
@@ -9,6 +9,11 @@ import kha.graphics2.Graphics;
     States are containers of game logic. For example, a menu state
     would describe a game's menu screen, while a play state would
     describe a game's gameplay screen.
+
+    A state handles the objects assigned to it, and also defines a
+    camera which 'observes' the objects. The camera's transformation
+    matrix is applied to the objects before they're drawn, so that
+    the objects are presented from the viewpoint of the camera.
 **/
 class State {
     /**
@@ -19,7 +24,7 @@ class State {
     /**
         State objects.
     **/
-    @:noCompletion var objects : Array<Object>;
+    var objects : Array<Object>;
 
     /**
         Creates new state.
@@ -64,9 +69,42 @@ class State {
     }
 
     /**
-        Adds object to state objects.
+        Adds object to the front of the state.
+        @param  object  Object
     **/
     inline function add(object : Object) : Void {
         objects.push(object);
+    }
+
+    /**
+        Adds object to the back of the state.
+        @param  object  Object
+    **/
+    inline function addToBack(object : Object) : Void {
+        objects.insert(0, object);
+    }
+
+    /**
+        Remove object from state. Object is not destroyed.
+        @param  object  Object
+    **/
+    inline function remove(object : Object) : Void {
+        objects.remove(object);
+    }
+
+    /**
+        Sends existing object to the front of the state.
+        @param  object  Object
+    **/
+    inline function sendToFront(object : Object) : Void {
+        if (objects.remove(object)) objects.push(object);
+    }
+
+    /**
+        Sends existing object to the back of the state.
+        @param  object  Object
+    **/
+    inline function sendToBack(object : Object) : Void {
+        if (objects.remove(object)) objects.insert(0, object);
     }
 }
