10b885bf959da7acf4965fed4f80b2094192cc4c
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-11-12 21:05:49 +0100

    Add Random class

---
+++ Sources/mokha/math/Random.hx
@@ -0,0 +1,39 @@
+package mokha.math;
+
+import kha.math.Random;
+
+class Random {
+    var random : kha.math.Random;
+
+    public function new(?seed : Int = 1) : Void {
+        this.random = new kha.math.Random(seed);
+    }
+
+    public inline function getInt() : Int {
+        return random.Get();
+    }
+
+    public inline function getIntIn(min : Int, max : Int) : Int {
+        return random.GetIn(min, max);
+    }
+
+    public inline function getIntUpTo(max : Int) : Int {
+        return random.GetUpTo(max);
+    }
+
+    public inline function getFloat() : Float {
+        return random.GetFloat();
+    }
+
+    public inline function getFloatIn(min : Float, max : Float) : Float {
+        return random.GetFloatIn(min, max);
+    }
+
+    public inline function getFloatUpTo(max : Float) : Float {
+        return getFloatIn(0, max);
+    }
+
+    public inline function check(probability : Float) : Bool {
+        return probability > getFloat();
+    }
+}
